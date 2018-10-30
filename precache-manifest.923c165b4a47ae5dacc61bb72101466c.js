42486eb35e87fc2695eff0bcac6ce41fa8bf69db
Dan Linehan <wagist@gmail.com>
2018-10-30 19:36:54 +0100

    Updates

---
+++ precache-manifest.923c165b4a47ae5dacc61bb72101466c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "1a5ba29a3a52495242ca",
+    "url": "/clicky-game/static/js/main.1a5ba29a.chunk.js"
+  },
+  {
+    "revision": "715a043cc0993a86cb15",
+    "url": "/clicky-game/static/js/1.715a043c.chunk.js"
+  },
+  {
+    "revision": "1a5ba29a3a52495242ca",
+    "url": "/clicky-game/static/css/main.db01bc62.chunk.css"
+  },
+  {
+    "revision": "8d75f09b76a4661519b9fb9ce712bcd4",
+    "url": "/clicky-game/index.html"
+  }
+];
